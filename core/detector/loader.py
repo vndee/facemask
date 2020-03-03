e1ae8035eb1b24f690188a6670332c0730ea1391
Duy Huynh <hvd.huynhduy@gmail.com>
2020-03-03 11:48:46 +0700

    Initial commit

---
+++ core/detector/loader.py
@@ -0,0 +1,12 @@
+import sys
+import torch
+
+
+def load_pytorch_model(model_path):
+    model = torch.load(model_path)
+    return model
+
+
+def pytorch_inference(model, img_arr):
+    y_bboxes, y_scores, = model.forward(torch.tensor(img_arr).float())
+    return y_bboxes.detach().numpy(), y_scores.detach().numpy()
